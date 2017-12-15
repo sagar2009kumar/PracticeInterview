17cc7d37708f673d6ebf94e7cf3c70a2738770a8
sagar2009kumar <sagar2009kumar@users.noreply.github.com>
2017-12-15 18:28:58 +0530

    Proof is also included

--- General/ModularInverse.cpp
+++ General/ModularInverse.cpp
@@ -3,6 +3,19 @@ using namespace std;
 typedef long long int ll;
 const int mod = 1e9+7;
 
+/* Fermat's little theorem 
+ * if a > 0 and p is positive and prime then
+ * a^(p-1) is 1 mod p
+ * then dividing by a both sides we get a^(p-2) is a^(-1) mod p
+ * this can be proof by the reducing fact.
+ * reducing fact tells that 
+ * a, 2a, 3a, ... (p-1)a is congruent to 
+ * 1, 2, 3, 4, ... (p-1) thus 
+ * a*2a*3a*4a*...(p-1)a is congruent to 1*2*3*... (p-1)
+ * thus cancelling 1*2*3*...(p-1) from both sides
+ * we get a^(p-1) is congruent to 1 mod p
+ */
+
 inline ll power(ll a, ll b) {
     ll res = 1ll;
     while(b > 0) {
